24ad9fda6b9e25866d6aecae793938540e142075
AyushiPohekar <ayushi2021.29@gmail.com>
2024-01-30 01:24:39 +0530

    integrated texttoimage,findModel,findAllmodel apis

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import "./App.css";
 import ModelsPage from "./Components/ModelsPage/ModelsPage";
 
 import Navbar from "./CommonComponents/Navbar/Navbar";
-import Aimodels from "./Components/Aimodels/Aimodels";
+
 import Footer from "./CommonComponents/Navbar/Footer/Footer";
 import TryModels from "./Components/TryModels/TryModels";
+import Type from "./Components/Type/Type";
+import TexttoImage from "./Components/Type/TexttoImage";
+import ImageToImage from "./Components/Type/ImageToImage";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<ModelsPage />} />
-        <Route path="/models" element={<ModelsPage />} />
-        <Route path="/trymodels" element={<TryModels />} />
+<Routes>
+      <Route path="/" element={<ModelsPage />} />
+      
+        {/* <Route path="/models" element={<ModelsPage />} /> */}
+        <Route path="/models" element={<TryModels />} />
+        <Route path="/models/type" element={<Type />} />
+        <Route path="/models/texttoImage/:name" element={<TexttoImage/>} />
+        <Route path="/models/imageToImage/:name" element={<ImageToImage />} />
          
-        <Route path="/models/:name" element={<Aimodels />} />
-      </Routes>
+     
+        </Routes>
       <Footer/>
     </>
   );
---
+++ src/Components/Type/ImageToImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageToImage = () => {
+  return (
+    <div>ImageToImage</div>
+  )
+}
+
+export default ImageToImage
