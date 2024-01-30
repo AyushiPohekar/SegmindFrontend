d25d8ee930f180b53e57324a429358a210558230
dibakar63 <110836157+dibakar63@users.noreply.github.com>
2024-01-30 16:18:50 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -3,31 +3,46 @@ import "./App.css";
 import ModelsPage from "./Components/ModelsPage/ModelsPage";
 
 import Navbar from "./CommonComponents/Navbar/Navbar";
-
+import Aimodels from "./Components/Aimodels/Aimodels";
 import Footer from "./CommonComponents/Navbar/Footer/Footer";
 import TryModels from "./Components/TryModels/TryModels";
-import Type from "./Components/Type/Type";
-import TexttoImage from "./Components/Type/TexttoImage";
-import ImageToImage from "./Components/Type/ImageToImage";
+import ApiKey from "./Components/Console/ApiKey";
+import Console from "./Components/Console/Console";
+import Usage from "./Components/Console/Usage";
+import Billing from "./Components/Console/Billing";
+import TrainingModel from "./Components/Console/TrainingModel";
+import TrainingHub from "./Components/Console/TrainingHub";
+import ModelHub from "./Components/Console/ModelHub";
+import Referalss from "./Components/Console/Referalss";
+
 
 function App() {
   return (
+    
     <>
-      <Navbar />
-<Routes>
+    <Navbar />
+
+    <Routes>
       <Route path="/" element={<ModelsPage />} />
-      
-        {/* <Route path="/models" element={<ModelsPage />} /> */}
-        <Route path="/models" element={<TryModels />} />
-        <Route path="/models/type" element={<Type />} />
-        <Route path="/models/texttoImage/:name" element={<TexttoImage/>} />
-        <Route path="/models/imageToImage/:name" element={<ImageToImage />} />
-         
      
-        </Routes>
-      <Footer/>
-    </>
+      <Route path="/models" element={<TryModels />} />
+       
+      <Route path="/models/:name" element={<Aimodels />} />
+      <Route path='/console/key' element={<ApiKey/>}/>
+      <Route path='/console/usage' element={<Usage/>}/>
+      <Route path='/console/billing' element={<Billing/>}/>
+      <Route path='/console/trainingModel' element={<TrainingModel/>}/>
+      <Route path='/console/hub' element={<TrainingHub/>}/>
+      <Route path='/console/modelhub' element={<ModelHub/>}/>
+      <Route path='/console/referals' element={<Referalss/>}/>
+
+    </Routes>
+    <Footer/>
+  </>
+    
+    
   );
 }
 
 export default App;
+
--- src/CommonComponents/Navbar/Navbar.jsx
+++ src/CommonComponents/Navbar/Navbar.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import "./Navbar.css"
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import { useState } from "react";
+// import 'bootstrap/dist/css/bootstrap.css';
+import Dropdown from "react-bootstrap/Dropdown";
+import { useNavigate, Link } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate=useNavigate();
+  const [toggle, setToggle] = useState(false);
+  const handlebtn = () => {
+    
+    setToggle(!toggle);
+    console.log(toggle);
+  };
+  const navigate = useNavigate();
   return (
-    <div className='NavbarDiv'>
-        <h1 onClick={()=>navigate("/")}>LegaciesAI</h1>
-        <div className='Navbardiv2'>
-        <h1 onClick={()=>navigate("/models")}>Models</h1>
-        <h1 onClick={()=>navigate("/models")}>Docs</h1>
-        <h1 onClick={()=>navigate("/models")}>Blog</h1>
-        <h1 onClick={()=>navigate("/models")} style={{marginRight:"35px"}}> G Login</h1>
-        </div>
-    </div>
-  )
-}
+    <div className="NavbarDiv">
+      <h1 onClick={() => navigate("/")}>LegaciesAI</h1>
+      <div className="Navbardiv2">
+        <h1 onClick={() => navigate("/models")}>Models</h1>
+        <h1 onClick={() => navigate("/models")}>Docs</h1>
+        <h1 onClick={() => navigate("/models")}>Blog</h1>
 
-export default Navbar
+        
+        <h1 onClick={handlebtn} style={{marginRight:"35px"}} > G Login </h1>
+        <br />
+
+        <div className="dropdown" onClick={handlebtn}>
+          {toggle ? (
+            <div className="dropdowndiv">
+              <h2 onClick={() => navigate("/console/key")}>Console</h2>
+              <h2 onClick={() => navigate("/models")}>Sign Out</h2>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
